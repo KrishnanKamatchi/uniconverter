2dc8b9ae9893bcb2da3b8e58cbecf0d024de9601
krishna <krishna@shravtek.com>
2024-02-25 12:11:49 +0530

    Initial Commit
    Initial Dart Flutter package

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(uniconverter());
+}
+
+class uniconverter extends StatelessWidget {
+  const uniconverter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
